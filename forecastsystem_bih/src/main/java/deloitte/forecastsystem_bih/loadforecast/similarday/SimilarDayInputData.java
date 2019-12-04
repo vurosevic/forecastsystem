fd9d391b738e5865c57a98e7cb7094840645b8da
PC_CNS <PC_CNS@192.168.92.237>
2019-12-04 14:06:23 +0100

    Added SimilarDayInputData...
    Not implemented yet

---
+++ forecastsystem_bih/src/main/java/deloitte/forecastsystem_bih/loadforecast/similarday/SimilarDayInputData.java
@@ -0,0 +1,37 @@
+package deloitte.forecastsystem_bih.loadforecast.similarday;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import deloitte.forecastsystem_bih.model.PreparedDataLoadHours;
+import deloitte.forecastsystem_bih.model.communication.PreparedDataLoadHoursRecord;
+import deloitte.forecastsystem_bih.service.PreparedDataLoadHoursService;
+
+public class SimilarDayInputData {
+	
+	@Autowired 
+	PreparedDataLoadHoursService preparedDataLoadHoursService;
+	
+	PreparedDataLoadHoursRecord inputDataForToday[];
+	PreparedDataLoadHoursRecord inputDataForTomorrow[];
+	
+	public SimilarDayInputData() {
+		// TODO Auto-generated constructor stub
+		inputDataForToday = new PreparedDataLoadHoursRecord[24];
+		inputDataForTomorrow = new PreparedDataLoadHoursRecord[24];
+	}
+	
+	public PreparedDataLoadHoursRecord[] prepareInputDataForToday(Date todayDate) {
+		
+		
+		return inputDataForToday;
+	}
+	
+	public PreparedDataLoadHoursRecord[] prepareInputDataForTomorrow(Date todayDate) {
+		
+		
+		return inputDataForTomorrow;
+	}	
+	
+}
