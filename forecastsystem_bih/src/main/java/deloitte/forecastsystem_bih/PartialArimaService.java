36f94c67e7a7341684e88921553f8eae7bb18777
PC_CNS <PC_CNS@192.168.92.237>
2019-12-09 15:20:24 +0100

    Added PartialArimaService 	  PartialSimilarDayService

---
+++ forecastsystem_bih/src/main/java/deloitte/forecastsystem_bih/PartialArimaService.java
@@ -0,0 +1,94 @@
+package deloitte.forecastsystem_bih;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import deloitte.forecastsystem_bih.loadforecast.arima.ArimaModelService;
+import deloitte.forecastsystem_bih.loadforecast.arima.ArimaRecord;
+import deloitte.forecastsystem_bih.model.Country;
+import deloitte.forecastsystem_bih.model.LoadForecastArima;
+import deloitte.forecastsystem_bih.service.CountryService;
+import deloitte.forecastsystem_bih.service.LoadForecastArimaService;
+import deloitte.forecastsystem_bih.service.PreparedDataLoadHoursService;
+
+//@SpringBootApplication(scanBasePackages={"deloitte.*"})
+public class PartialArimaService implements CommandLineRunner {
+	
+	@Autowired 
+	PreparedDataLoadHoursService preparedDataLoadHoursService;
+	
+	@Autowired
+	CountryService countryService; 
+	
+	@Autowired
+	ArimaModelService arimaModelService;	
+	
+	@Autowired
+	LoadForecastArimaService loadForecastArimaService;		
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PartialArimaService.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("PARTIAL ARIMA SERVICE: ");
+		
+		Country con = countryService.findById(2L);
+		
+		double[] res = preparedDataLoadHoursService.getAllRealDataLoadHoursByCountry(con);
+		int[] res_god = preparedDataLoadHoursService.getAllGodinaLoadHoursByCountry(con);
+		int[] res_mes = preparedDataLoadHoursService.getAllMesecLoadHoursByCountry(con);
+		int[] res_dan = preparedDataLoadHoursService.getAllDanLoadHoursByCountry(con);
+		int[] res_hour = preparedDataLoadHoursService.getAllHourLoadHoursByCountry(con);
+		
+		Long startPos = preparedDataLoadHoursService.getMinIndexForPartialData(con);
+		Long endPos = preparedDataLoadHoursService.getMaxIndexForPartialData(con);
+		
+		if (startPos == null) {
+			System.out.println("No data for update...");
+			return;
+		}
+		
+		arimaModelService.setDataArray(res);
+		arimaModelService.setpForecastSize(1);		
+		
+		arimaModelService.setPp(24); 
+		arimaModelService.setPd(1); 
+		arimaModelService.setPq(1); 
+		
+        Calendar c = Calendar.getInstance();
+		LoadForecastArima lfa = new LoadForecastArima();		
+		
+		for (int i=startPos.intValue(); i<res.length; i++) {
+					arimaModelService.prepareDataArrayPart(i);
+					arimaModelService.trainArima();		
+					
+					System.out.println(i + ".  "+arimaModelService.getDataArray()[i] + "," + arimaModelService.getForecastData()[0]);
+					
+			        c.set(res_god[i], res_mes[i]-1, res_dan[i], 0, 0, 0);	
+			        ArimaRecord arimaRec = new ArimaRecord(c.getTime(), res_hour[i], new Date());
+					
+					arimaRec.addHourForecast(arimaModelService.getForecastData()[0]);
+					lfa.setCountry(countryService.findById(2L));
+					lfa.setLoadDate(arimaRec.getLoadDate());
+					lfa.setForecastDate(arimaRec.getForecastDate());
+					lfa.setLoadForecastArima(arimaRec.getLoadForecastArima());
+					lfa.setLoadHour(arimaRec.getLoadHour());
+					lfa.setLoadMinute(arimaRec.getLoadMinute());
+					lfa.setId(0L); 
+					
+					loadForecastArimaService.save(lfa);					
+		}
+		
+		System.out.println("End...");
+	}
+
+}
---
+++ forecastsystem_bih/src/main/java/deloitte/forecastsystem_bih/PartialSimilarDayService.java
@@ -0,0 +1,89 @@
+package deloitte.forecastsystem_bih;
+
+import java.util.Calendar;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import deloitte.forecastsystem_bih.loadforecast.similarday.SimilarDayService;
+import deloitte.forecastsystem_bih.model.Country;
+import deloitte.forecastsystem_bih.model.LoadForecastSimilarDay;
+import deloitte.forecastsystem_bih.model.PreparedDataLoadHours;
+import deloitte.forecastsystem_bih.model.communication.PreparedDataLoadHoursRecord;
+import deloitte.forecastsystem_bih.service.CountryService;
+import deloitte.forecastsystem_bih.service.LoadForecastSimilarDayService;
+import deloitte.forecastsystem_bih.service.PreparedDataLoadHoursService;
+
+//@SpringBootApplication(scanBasePackages={"deloitte.*"})
+public class PartialSimilarDayService implements CommandLineRunner {
+
+	@Autowired
+	CountryService countryService;
+	
+	@Autowired
+	SimilarDayService similarDayService;
+
+	@Autowired 
+	LoadForecastSimilarDayService loadForecastSimilarDayService; 		
+	
+	@Autowired 
+	PreparedDataLoadHoursService preparedDataLoadHoursService;		
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PartialSimilarDayService.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("PARTIAL SIMILAR DAY SERVICE: ");
+		
+		Country con = countryService.findById(2L);	
+		
+		LoadForecastSimilarDay lfsd = new LoadForecastSimilarDay();
+		
+		similarDayService.set(con, 16172L);
+		similarDayService.normalizeData();	
+		
+		Long startPos = preparedDataLoadHoursService.getMinIndexForPartialData(con);
+		Long endPos = preparedDataLoadHoursService.getMaxIndexForPartialData(con);
+		
+		if (startPos == null) {
+			System.out.println("No data for update...");
+			return;
+		}
+		
+		for (Long number = startPos; number < endPos; number++) {
+		
+		PreparedDataLoadHours recData = preparedDataLoadHoursService.findById(number).get();
+		
+		similarDayService.set(con, number-1);		
+		
+		PreparedDataLoadHoursRecord rec = new PreparedDataLoadHoursRecord(number, recData.getAvgTemperature4() , recData.getAvgFeelslike4() , recData.getAvgLoadRealData4(), 
+																			  	  recData.getAvgTemperature3() , recData.getAvgFeelslike3() , recData.getAvgLoadRealData3(), 
+																			  	  recData.getAvgTemperature2() , recData.getAvgFeelslike2() , recData.getAvgLoadRealData2(), 
+																			  	  recData.getAvgLoadRealData());		
+		similarDayService.calculateDistance(rec); 
+			
+		lfsd.setCountry(con);
+		lfsd.setId(0L);
+		Calendar c = Calendar.getInstance();
+		lfsd.setForecastDate(c.getTime());
+		c.set(recData.getGodina(), recData.getMesec()-1, recData.getDan(), 0, 0, 0);
+		lfsd.setLoadDate(c.getTime());
+		lfsd.setLoadForecastSimilarDay(similarDayService.getForecast()); 
+		lfsd.setLoadHour(recData.getLoadHour());
+		lfsd.setLoadMinute(0); 
+		loadForecastSimilarDayService.save(lfsd);
+		
+		System.out.println(recData.getAvgLoadRealData() + "," + similarDayService.getForecast());
+		
+		} // number
+		
+		System.out.println("....... END .......");		
+	}
+	
+}
